5d4f04c4c11cbb0414cf48118a3ac61db221f0c0
conumaju <conumaju@users.noreply.github.com>
2020-03-10 10:02:19 -0400

    Update main.js

--- lab_5/scripts/main.js
+++ lab_5/scripts/main.js
@@ -2,3 +2,13 @@ let cont = document.querySelector('.content');
 let newOL = document.createElement('ol');
 cont.append(newOL);
 newOL.className = "countries";
+
+let btn = document.querySelector('button');
+
+for (let i=0; i < countries.length; i++) {
+    
+}
+
+btn.onclick = function() {
+
+}
